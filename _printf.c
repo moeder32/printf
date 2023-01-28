2671b69861e39e64d92c9483ee984f31179d9225
Goodness <atundeg@gmail.com>
2023-01-28 01:35:10 +0100

    add the custom printf function, _printf

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * _printf - function to print anything
+ * @format: types of argument passed to the function
+ *
+ *  Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	int check = 0, i;
+	va_list arguments;
+	int (*func)(va_list);
+
+	va_start(arguments, format);
+
+	if (format == NULL)
+		return (-1);
+
+	for (i = 0; format[i]; i++)
+	{
+			if (!(format[i]))
+				return (-1);
+		if (format[i] == '%')
+		{
+			i++;
+
+			func = get_flag_func(format[i]);
+
+			if (func == NULL)
+			{
+				_write('%');
+				_write(format[i]);
+				check += 2;
+			}
+			else
+				check += func(arguments);
+		}
+		else
+		{
+			_write(format[i]);
+			check++;
+		}
+	}
+	va_end(arguments);
+	return (check);
+}
